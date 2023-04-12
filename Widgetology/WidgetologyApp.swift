99f0902d1bd116313e0e37b660d9c2a3bc040c79
ToastArgumentative <toastarg@icloud.com>
2023-04-12 17:29:24 -0400

    Initial Commit

---
+++ Widgetology/WidgetologyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetologyApp.swift
+//  Widgetology
+//
+//  Created by Austin Aleshire on 4/12/23.
+//
+
+import SwiftUI
+
+@main
+struct WidgetologyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
