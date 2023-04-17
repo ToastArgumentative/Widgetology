3dd760f2178267f1d5787ce4ad3db4b89da2a703
ToastArgumentative <toastarg@icloud.com>
2023-04-17 17:11:48 -0400

    init commit

---
+++ WidgetologyWidgets/WidgetologyWidgetsBundle.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetologyWidgetsBundle.swift
+//  WidgetologyWidgets
+//
+//  Created by Austin Aleshire on 4/17/23.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct WidgetologyWidgetsBundle: WidgetBundle {
+    var body: some Widget {
+        WidgetologyWidgets()
+        WidgetologyWidgetsLiveActivity()
+    }
+}
